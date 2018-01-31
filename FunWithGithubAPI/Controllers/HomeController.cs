77d6d5deeaffb082653a9bf2ed86f9f203c04f19
DaveKOregon <davek@dsandfamily.com>
2018-01-31 02:52:37 -0800

    Add project files.

---
+++ FunWithGithubAPI/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Microsoft.AspNetCore.Mvc;
+using FunWithGithubAPI.ViewModels;
+
+namespace FunWithGithubAPI.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+        public IActionResult Index()
+        {
+            string repoJson = null;
+
+            HttpWebRequest webReq = (HttpWebRequest)WebRequest.Create("https://api.github.com/users/davekoregon/repos");
+            webReq.Headers = new WebHeaderCollection() { "User-Agent: DaveKOregon" };
+
+            using (WebResponse webResp = webReq.GetResponse())
+            {
+                using (Stream str = webResp.GetResponseStream())
+                {
+                    using (StreamReader sRdr = new StreamReader(str))
+                    {
+                        repoJson = sRdr.ReadToEnd();
+                    }
+                }
+            }
+
+            IEnumerable<HomeViews.Repo> repoData = JsonConvert.DeserializeObject<HomeViews.Repo[]>(repoJson);
+
+            HomeViews.Index Index = new HomeViews.Index()
+            {
+                Repos = repoData
+                    .OrderByDescending(r => r.stargazers_count)
+                    .ThenBy(r => r.name)
+                    .Take(5)
+                    .ToList()
+            };
+
+            return View(Index);
+        }
+    }
+}
---
+++ FunWithGithubAPI/ViewModels/HomeViews.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FunWithGithubAPI.ViewModels
+{
+    public class HomeViews
+    {
+        public class Repo
+        {
+            public string id { get; set; }
+            public string name { get; set; }
+            public string full_name { get; set; }
+            public string html_url { get; set; }
+            public int stargazers_count { get; set; }
+        }
+
+        public class Index
+        {
+            public List<Repo> Repos { get; set; }
+        }
+    }
+}
